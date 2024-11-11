b9e81f9666d4781399d7af750d827cd035f47ca9
ShreyasAndurlekar <and.shreyas@gmail.com>
2024-11-11 10:17:27 +0530

    Roadmap fix

--- src/pages/HomeOne.jsx
+++ src/pages/HomeOne.jsx
@@ -27,82 +27,91 @@ function HomeOne(props) {
            {<Project3 data={dataProject}/>}
 
 
-            <section id='roadmap' className="tf-section roadmap">
-        <div className="container">
-            <div className="row"> 
-
+           <section id='roadmap' className="tf-section roadmap">
+    <div className="container">
+        <div className="row"> 
             <div className="col-md-12">
-                            <div className="tf-title mb40" data-aos="fade-up" data-aos-duration="800">
-                                <h2 className="title">
-                                    Roadmap
-                                </h2>
+                <div className="tf-title mb40" data-aos="fade-up" data-aos-duration="800">
+                    <h2 className="title">
+                        Roadmap
+                    </h2>
+                </div>
+            </div>
+            <div className="col-md-12"> 
+                <div className="roadmap-wrapper-style2" data-aos="fade-up" data-aos-duration="1200">
+                    <div className="left">
+                        <div className="roadmap-box-style active">
+                            <div className="icon">
+                                <img src={icon} alt="" />
+                            </div>
+                            <div className="content">
+                                <h6 className="date">November 21, 2024</h6>
+                                <ul>
+                                    <li>Round 1: Online </li>
+                                    <li>PPt Submission</li>
+                                    <li>Registration Start Date</li>
+                                </ul>
                             </div>
                         </div>
-                <div className="col-md-12"> 
-                    <div className="roadmap-wrapper-style2" data-aos="fade-up" data-aos-duration="1200">
-                        <div className="left">
-                            <div className="roadmap-box-style active">
-                                <div className="icon">
-                                    <img src={icon} alt="" />
-                                </div>
-                                <div className="content">
-                                    <h6 className="date">November 21, 2024</h6>
-                                    <ul>
-                                        <li>Round 1: Online </li>
-                                        <li>PPt Submission</li>
-                                        <li>Registration Start Date</li>
-                                    </ul>
-                                </div>
+                        <div className="roadmap-box-style active">
+                            <div className="icon">
+                                <img src={icon} alt="" />
                             </div>
-                            <div className="roadmap-box-style active">
-                                <div className="icon">
-                                    <img src={icon} alt="" />
-                                </div>
-                                <div className="content">
-                                    <h6 className="date">December 11-15, 2024</h6>
-                                    <ul>
-                                        <li>Round 1 : Evalution</li>
-                                        <li>Announcement of Finalist</li>
-                                        
-                                    </ul>
-                                </div>
+                            <div className="content">
+                                <h6 className="date">December 11-15, 2024</h6>
+                                <ul>
+                                    <li>Round 1 : Evalution</li>
+                                    <li>Announcement of Finalist</li>
+                                </ul>
                             </div>
-
-                    
                         </div>
-                        <div className="right">
-                            <div className="roadmap-box-style active">
-                                <div className="icon">
-                                    <img src={icon} alt="" />
-                                </div>
-                                <div className="content">
-                                    <h6 className="date"> December 10, 2024</h6>
-                                    <ul>
-                                        <li>Round 1: Online</li>
-                                        <li>PPT Submission Deadline</li>
 
-                                    </ul>
-                                </div>
+                        {/* New Block */}
+                        <div className="roadmap-box-style active">
+                            <div className="icon">
+                                <img src={icon} alt="" />
                             </div>
-                            <div className="roadmap-box-style active">
-                                <div className="icon">
-                                    <img src={icon} alt="" />
-                                </div>
-                                <div className="content">
-                                    <h6 className="date">January 10, 2025</h6>
-                                    <ul>
-                                        <li>Round 2: Offline</li>
-                                        <li>24 hr Hackathon Kickoff</li>
-                                    </ul>
-                                </div>
+                            <div className="content">
+                                <h6 className="date">January 11, 2025</h6>
+                                <ul>
+                                    <li>Final Judging</li>
+                                    <li>Valedictory Ceremony</li>
+                                </ul>
                             </div>
-                         
                         </div>
                     </div>
-                </div> 
-            </div>
+                    <div className="right">
+                        <div className="roadmap-box-style active">
+                            <div className="icon">
+                                <img src={icon} alt="" />
+                            </div>
+                            <div className="content">
+                                <h6 className="date">December 10, 2024</h6>
+                                <ul>
+                                    <li>Round 1: Online</li>
+                                    <li>PPT Submission Deadline</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="roadmap-box-style active">
+                            <div className="icon">
+                                <img src={icon} alt="" />
+                            </div>
+                            <div className="content">
+                                <h6 className="date">January 10, 2025</h6>
+                                <ul>
+                                    <li>Round 2: Offline</li>
+                                    <li>24 hr Hackathon Kickoff</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> 
         </div>
-    </section>
+    </div>
+</section>
+
 
     {/* <div className="roadmap-box-style active">
                                 <div className="icon">
