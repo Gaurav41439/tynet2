3c05880b550da87f04a02ee6fac29d7ff447d40f
Nishita Kapkar <nishitakapkar15@gmail.com>
2024-11-12 20:35:13 +0530

    Added Faq Content

--- src/pages/PageFaqs.jsx
+++ src/pages/PageFaqs.jsx
@@ -16,7 +16,7 @@ function PageFaqs(props) {
         
         {<Faqs data={dataFaq} />} 
 
-    {/* <section className="tf-section FAQs">
+    {<section className="tf-section FAQs">
         <div className="container ">
             <div className="row"> 
                 <div className="col-md-12">
@@ -25,55 +25,55 @@ function PageFaqs(props) {
                             <div className="h7 toggle-title">
                                 <span className="icon-Icon">
                                 </span>
-                                <span>What is the metaverse?</span>
+                                <span>Who can participate in TYNET 2.0?</span>
                             </div>
-                            <p className="toggle-content" >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus eleifend egestas. Duis eget dictum purus. Fusce est mi, imperdiet porta neque malesuada, blandit scelerisque lectus. Maecenas vel lobortis risus. Donec ornare urna quis libero euismod, ac fermentum diam blandit. Nunc dapibus tempor lacus, vel lacinia enim efficitur eu</p>
+                            <p className="toggle-content" >TYNET 2.0 is an international, women-centric hackathon. We welcome women passionate about technology from all skill levels and backgrounds to participate. Male allies can join as mentors or volunteers to support the event.</p>
                         </div>
                         <div className="flat-toggle active">
                             <div className="h7 toggle-title active">
                                 <span className="icon-Icon">
                                 </span>
-                                <span>How to access the metaverse?</span>
+                                <span>What is the format of TYNET 2.0?</span>
                             </div>
-                            <p className="toggle-content" >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus eleifend egestas. Duis eget dictum purus. Fusce est mi, imperdiet porta neque malesuada, blandit scelerisque lectus. Maecenas vel lobortis risus. Donec ornare urna quis libero euismod, ac fermentum diam blandit. Nunc dapibus tempor lacus, vel lacinia enim efficitur eu</p>
+                            <p className="toggle-content" >TYNET 2.0 consists of two rounds. The first round is an online submission round where participants submit their project ideas. Selected teams will then advance to the final, 24-hour offline hackathon held at RAIT.</p>
                         </div>
                         <div className="flat-toggle">
                             <div className="h7 toggle-title">
                                 <span className="icon-Icon">
                                 </span>
-                                <span>How do you make purchases in the metaverse?</span>
+                                <span>How can I register for TYNET 2.0?</span>
                             </div>
-                            <p className="toggle-content" >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus eleifend egestas. Duis eget dictum purus. Fusce est mi, imperdiet porta neque malesuada, blandit scelerisque lectus. Maecenas vel lobortis risus. Donec ornare urna quis libero euismod, ac fermentum diam blandit. Nunc dapibus tempor lacus, vel lacinia enim efficitur eu</p>
+                            <p className="toggle-content" >You can register on platforms like Unstop and DevPost. Follow the registration guidelines and submit the required details. Registration is open from 21st November 2024 to 10th December 2024.</p>
                         </div>
                         <div className="flat-toggle">
                             <div className="h7 toggle-title">
                                 <span className="icon-Icon">
                                 </span>
-                                <span>Is the metaverse fun?</span>
+                                <span>What resources are provided during the hackathon?</span>
                             </div>
-                            <p className="toggle-content" >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus eleifend egestas. Duis eget dictum purus. Fusce est mi, imperdiet porta neque malesuada, blandit scelerisque lectus. Maecenas vel lobortis risus. Donec ornare urna quis libero euismod, ac fermentum diam blandit. Nunc dapibus tempor lacus, vel lacinia enim efficitur eu</p>
+                            <p className="toggle-content" >RAIT ACM-W will provide participants with essential event materials, access to mentors, high-speed internet, a comfortable workspace, and refreshments throughout the event.</p>
                         </div>
                         <div className="flat-toggle">
                             <div className="h7 toggle-title">
                                 <span className="icon-Icon">
                                 </span>
-                                <span>How do I find things to actually do in the metaverse?</span>
+                                <span>Is there a participation fee?</span>
                             </div>
-                            <p className="toggle-content" >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus eleifend egestas. Duis eget dictum purus. Fusce est mi, imperdiet porta neque malesuada, blandit scelerisque lectus. Maecenas vel lobortis risus. Donec ornare urna quis libero euismod, ac fermentum diam blandit. Nunc dapibus tempor lacus, vel lacinia enim efficitur eu</p>
+                            <p className="toggle-content" >Round 1 of TYNET 2.0 is completely free. However, if your team is selected for Round 2, a small participation fee will be required to cover event amenities, including meals and refreshments, which will be provided at no additional cost during the 24-hour hackathon.</p>
                         </div>
                         <div className="flat-toggle">
                             <div className="h7 toggle-title">
                                 <span className="icon-Icon">
                                 </span>
-                                <span>What is ownership in the metaverse?</span>
+                                <span>Do I need to have a team to register, or can I participate individually?</span>
                             </div>
-                            <p className="toggle-content" >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus eleifend egestas. Duis eget dictum purus. Fusce est mi, imperdiet porta neque malesuada, blandit scelerisque lectus. Maecenas vel lobortis risus. Donec ornare urna quis libero euismod, ac fermentum diam blandit. Nunc dapibus tempor lacus, vel lacinia enim efficitur eu</p>
+                            <p className="toggle-content" >To participate in TYNET 2.0, you must register with a team of 2 to 4 members. Individual registrations are not allowed, so please ensure your team is complete before signing up.</p>
                         </div>
                     </div>
                 </div> 
             </div>
         </div>
-    </section>  */}
+    </section>}
 
     <section className="tf-section questions"> 
         <div className="container">
